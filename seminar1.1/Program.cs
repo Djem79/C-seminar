2fb6473f7162cd917da5ed827214024e794f15ad
Djem79 <tdm.979@gmail.com>
2023-04-20 11:57:12 +0300

    Решение задачи 7

--- seminar1.1/Program.cs
+++ seminar1.1/Program.cs
@@ -22,7 +22,7 @@ Console.WriteLine($"Your number is {number1}. Square of this number is {result}"
 // Напишите программу, которая на вход принимает 2 числа и проверяет,
 //является ли первое число квадратом второго.
 
-Console.WriteLine("Input your first number:");
+/*Console.WriteLine("Input your first number:");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input your second number:");
 int number2 = Convert.ToInt32(Console.ReadLine());
@@ -35,4 +35,46 @@ else
 {   
     Console.WriteLine($"Your first number is not a square of the second one");
 }
+*/
 
+//Задача 5.
+//Напишите программу, которая на вход принимает одно число (N), 
+//а на выходе показывает все целые числа в промежутке от -N до N.
+
+
+/*Console.WriteLine("Input your number:");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+int current_i = numN * (-1);
+
+while (current_i <= numN)
+{
+    Console.Write(current_i + " ");
+    current_i ++;
+}
+*/
+
+
+//Задача 7.
+//Напишите программу, которая принимает на вход трёхзначное число и
+// на выходе показывает последнюю цифру этого числа.
+
+
+//456 % 10 -> 6
+//456 % 100 -> 56
+//456 / 10 -> 45
+//456 / 100 -> 4
+
+Console.WriteLine("Input your number:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 99 && num < 1000)
+{   
+    //int dev = num % 10;
+    //Console.WriteLine($"Last digit {num} is {dev}");
+    Console.WriteLine("Last num is: " + num%10);
+}
+else
+{
+    Console.WriteLine ("Your number is not 3 digits");
+}
