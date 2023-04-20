0135c4bfea82ac0eb33657cbbe8282966525063e
Djem79 <tdm.979@gmail.com>
2023-04-20 11:04:44 +0300

    Square number

--- seminar1.1/Program.cs
+++ seminar1.1/Program.cs
@@ -1,4 +1,17 @@
-﻿Console.WriteLine("Good morning");
+﻿/*Console.WriteLine("Good morning");
 Console.WriteLine("Input your number: ");
 int number = Convert.ToInt32(Console.ReadLine ());
 Console.WriteLine(number);
+*/
+
+//Задача 1.
+//Напишите программу, которая на вход принимает число и выдает его квадрат
+
+Console.WriteLine("Input your number:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+int result = number1 * number1;
+
+//Console.WriteLine(number1 * number1);
+Console.WriteLine("Your number is " + number1 + " Square of this number is " + result);
+Console.WriteLine($"Your number is {number1}. Square of this number is {result}");
