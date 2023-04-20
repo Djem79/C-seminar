6f97499528272fb1263dea412f9f733caee73e08
Djem79 <tdm.979@gmail.com>
2023-04-20 11:23:43 +0300

    Решение задачи 3

--- seminar1.1/Program.cs
+++ seminar1.1/Program.cs
@@ -7,7 +7,7 @@ Console.WriteLine(number);
 //Задача 1.
 //Напишите программу, которая на вход принимает число и выдает его квадрат
 
-Console.WriteLine("Input your number:");
+/*Console.WriteLine("Input your number:");
 int number1 = Convert.ToInt32(Console.ReadLine());
 
 int result = number1 * number1;
@@ -15,3 +15,24 @@ int result = number1 * number1;
 //Console.WriteLine(number1 * number1);
 Console.WriteLine("Your number is " + number1 + " Square of this number is " + result);
 Console.WriteLine($"Your number is {number1}. Square of this number is {result}");
+*/
+
+
+//Задача 3.
+// Напишите программу, которая на вход принимает 2 числа и проверяет,
+//является ли первое число квадратом второго.
+
+Console.WriteLine("Input your first number:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input your second number:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number2 * number2 == number1)
+{
+    Console.WriteLine($"Your first number is a square of the second one");
+}
+else
+{   
+    Console.WriteLine($"Your first number is not a square of the second one");
+}
+
